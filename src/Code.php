bc005d4e711406d8a21c41dbd59a8c2175b39d94
Oleg Kasyanov <dev@mobicms.net>
2020-08-29 13:26:15 +0400

    Added parameter check

--- src/Code.php
+++ src/Code.php
@@ -12,46 +12,53 @@ declare(strict_types=1);
 
 namespace Mobicms\Captcha;
 
+use InvalidArgumentException;
+
 class Code
 {
     private int $lengthMin;
-
     private int $lengthMax;
+    private string $characterSet;
 
-    private string $letters;
-
-    /**
-     * @param int $lengthMin The minimum length of CAPTCHA code
-     * @param int $lengthMax The maximum length of CAPTCHA code
-     * @param string $letters The set of letters used in CAPTCHA
-     */
     public function __construct(
         int $lengthMin = 3,
         int $lengthMax = 5,
-        string $letters = '23456789ABCDEGHKMNPQSUVXYZabcdeghkmnpqsuvxyz'
+        string $characterSet = '23456789ABCDEGHKMNPQSUVXYZabcdeghkmnpqsuvxyz'
     ) {
+        if ($lengthMin < 1 || $lengthMax > 20) {
+            throw new InvalidArgumentException(
+                'Allowed parameter value from 1 to 20'
+            );
+        }
+
+        if ($lengthMax < $lengthMin) {
+            throw new InvalidArgumentException(
+                'The maximum length should not be less than the minimum'
+            );
+        }
+
+        if (empty($characterSet)) {
+            throw new InvalidArgumentException(
+                'Character set cannot be empty'
+            );
+        }
+
         $this->lengthMin = $lengthMin;
         $this->lengthMax = $lengthMax;
-        $this->letters = $letters;
+        $this->characterSet = $characterSet;
     }
 
-    /**
-     * @throws \Throwable
-     */
     public function __toString(): string
     {
         return $this->generate();
     }
 
-    /**
-     * @throws \Throwable
-     */
     public function generate(): string
     {
-        $length = random_int($this->lengthMin, $this->lengthMax);
+        $length = mt_rand($this->lengthMin, $this->lengthMax);
 
         do {
-            $code = substr(str_shuffle(str_repeat($this->letters, 3)), 0, $length);
+            $code = substr(str_shuffle(str_repeat($this->characterSet, 3)), 0, $length);
         } while (preg_match('/cp|cb|ck|c6|c9|rn|rm|mm|co|do|cl|db|qp|qb|dp|ww/', $code));
 
         return $code;
