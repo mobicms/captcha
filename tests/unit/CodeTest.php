f25c67de16688d3da36417d4a2672cb46e005905
Oleg Kasyanov <dev@mobicms.net>
2020-08-29 13:26:50 +0400

    Added missing tests

--- tests/unit/CodeTest.php
+++ tests/unit/CodeTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace MobicmsTest\Captcha;
 
 use Exception;
+use InvalidArgumentException;
 use Mobicms\Captcha\Code;
 use PHPUnit\Framework\TestCase;
 
@@ -36,16 +37,48 @@ class CodeTest extends TestCase
     public function testCanSpecifyLength(): void
     {
         $code = (string) new Code(2, 2);
-        $this->assertSame(2, strlen($code));
+        $this->assertEquals(2, strlen($code));
         $code = (string) new Code(5, 5);
-        $this->assertSame(5, strlen($code));
+        $this->assertEquals(5, strlen($code));
     }
 
     public function testCanSpecifyLetters(): void
     {
         $code = (string) new Code(3, 3, 'aaaaa');
-        $this->assertSame('aaa', $code);
+        $this->assertEquals('aaa', $code);
         $code = (string) new Code(3, 3, 'bb');
-        $this->assertSame('bbb', $code);
+        $this->assertEquals('bbb', $code);
+    }
+
+    /**
+     * @dataProvider invalidValues
+     * @param int $lengthMin
+     * @param int $lengthMax
+     * @param string $characterSet
+     */
+    public function testInvalidParameterValues(
+        int $lengthMin,
+        int $lengthMax,
+        string $characterSet
+    ): void {
+        $this->expectException(InvalidArgumentException::class);
+        new Code($lengthMin, $lengthMax, $characterSet);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Auxiliary methods                                                          //
+    ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @return array<string, array<int, int, string>>
+     */
+    public function invalidValues(): array
+    {
+        return [
+            'Minimum length value less than 1'        => [0, 5, 'abcd'],
+            'Maximum length value is greater than 20' => [3, 25, 'abcd'],
+            'Minimum length is greater than maximum'  => [5, 3, 'abcd'],
+            'Empty character set'                     => [3, 5, ''],
+        ];
     }
 }
