a85c7ca0874fc54fa75748032214cf9fac3bd512
Oleg Kasyanov <oleg@batumi.org>
2021-07-21 02:12:20 +0400

    Test suite refactoring

--- tests/stubs/constants.php
+++ tests/stubs/constants.php
@@ -10,5 +10,5 @@
 
 declare(strict_types=1);
 
-const TEST_OUT_PATH = __DIR__ . '/../tmp/';
-const TEST_FONTS_PATH = __DIR__ . '/../fonts/';
+const TEST_OUT_PATH = __DIR__ . '/';
+const TEST_FONTS_PATH = __DIR__ . '/';
